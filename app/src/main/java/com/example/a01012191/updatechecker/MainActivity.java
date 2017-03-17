902a46d23b4448fde0d0ea5c68d2172ed8be09ad
sekiyama-hidemitsu <h_sekiyama@r.recruit.co.jp>
2017-03-17 15:47:18 +0900

    最初の環境構築

---
+++ app/src/main/java/com/example/a01012191/updatechecker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a01012191.updatechecker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
